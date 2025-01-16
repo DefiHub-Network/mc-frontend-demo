b1b801cad42162a116785e64e60f222a2d279d9e
haphan <huuhaphan@hotmail.com>
2025-01-16 23:53:29 +0700

    iframe

--- src/views/payment/iframe.vue
+++ src/views/payment/iframe.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import WebApp from "@twa-dev/sdk";
 import { onBeforeUnmount, onMounted, ref } from "vue";
-
+import { useRoute } from "vue-router";
+const route = useRoute();
 const height = ref(window.innerHeight + "px");
 const updateSize = () => {
   height.value = window.innerHeight + "px";
@@ -10,10 +11,13 @@ document.addEventListener("resize", updateSize);
 onBeforeUnmount(() => {
   document.addEventListener("resize", updateSize);
 });
+
+console.log("WebApp.initData", WebApp.initData);
+console.log("route.initData", route);
 </script>
 <template>
   <iframe
-    :src="'https://appdev.defihub.network/trade/swap#' + WebApp.initDataUnsafe"
+    :src="'https://appdev.defihub.network/trade/swap#' + WebApp.initData"
     :style="{ width: '100%', height: '500px', border: 0 }"
   ></iframe>
 </template>
