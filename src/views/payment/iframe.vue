fbcb7e37ae2b1cfb0c1e0ee2e28095defeec100f
haphan <huuhaphan@hotmail.com>
2025-01-16 21:43:22 +0700

    iframe

--- src/views/payment/iframe.vue
+++ src/views/payment/iframe.vue
@@ -10,12 +10,10 @@ document.addEventListener("resize", updateSize);
 onBeforeUnmount(() => {
   document.addEventListener("resize", updateSize);
 });
-
 </script>
 <template>
-  
   <iframe
-    src="https://815f-2405-4802-38-b740-ad55-c0da-b2df-d010.ngrok-free.app/trade/swap"
+    src="https://c967-2405-4802-38-b740-ad55-c0da-b2df-d010.ngrok-free.app/trade/swap"
     :style="{ width: '100%', height: '500px', border: 0 }"
   ></iframe>
 </template>
